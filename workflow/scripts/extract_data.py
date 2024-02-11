48d95d7e84e2d9259dcfc6282de416ac49716bfa
trevorb1 <trevor_barnes@sfu.ca>
2024-02-11 11:00:51 -0800

    general updates

--- workflow/scripts/extract_data.py
+++ workflow/scripts/extract_data.py
@@ -1,7 +1,31 @@
-"""Module to extract txt file data from xlsx SDK"""
+"""Module to extract txt file data from xlsx SDK
 
-def main():
-    pass 
+This is the python equivalent to the VBA macro in the starter datakit files.
+Note, takes about 15-20sec per file
+"""
+
+from openpyxl import load_workbook
+import sys
+
+def excel_to_text(xlsm: str, out_file: str):
+    wb = load_workbook(xlsm, data_only=True, keep_links=False, read_only=True)
+    ws = wb["ToDataFile"]
+
+    with open(out_file, "w") as f:
+        for row in ws.iter_rows(values_only=True):
+            data = " ".join(str(cell) for cell in row if cell is not None)
+            f.write(f"{data}\n")
 
 if __name__ == "__main__":
-    main()
+    
+    if "snakemake" in globals():
+        excel_to_text(snakemake.input.xlsm, snakemake.output.txt)
+    else:
+        if len(sys.argv) != 3:
+            msg = "Usage: python {} <xlsm_file_in> <txt_file_out>"
+            print(msg.format(sys.argv[0]))
+            sys.exit(1)
+        else:
+            xlsm = sys.argv[1]
+            txt_file = sys.argv[2]
+            excel_to_text(xlsm, txt_file)
