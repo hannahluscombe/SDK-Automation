7507c53e41ef1610292b03461c5d3c4f6f1bbaf3
hannahluscombe <157520297+hannahluscombe@users.noreply.github.com>
2024-02-15 18:02:40 +0000

    Updates

--- workflow/scripts/download_xlsm.py
+++ workflow/scripts/download_xlsm.py
@@ -1,7 +1,31 @@
 """Module to download Zenodo SDK xlsx files"""
+import requests
 
-def main():
-    pass 
+import sys
+
+def download_file(url, file_path):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Open the file in binary write mode and write the content of the response to it
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        print(f"File downloaded successfully to {file_path}")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
 
 if __name__ == "__main__":
-    main()
+
+    if "snakemake" in globals():
+        download_file(snakemake.params.url, snakemake.output.xlsm)
+    else:
+        if len(sys.argv) != 3:
+            msg = "Usage: python {} <url> <xlsm_file>"
+            print(msg.format(sys.argv[0]))
+            sys.exit(1)
+        else:
+            url=sys.argv[1]
+            xlsm = sys.argv[2]
+            download_file(url, xlsm)
---
+++ workflow/scripts/simplify.py
@@ -0,0 +1,119 @@
+"""Removes default values from data file 
+
+Code taken from https://github.com/ClimateCompatibleGrowth/clicSANDMac/blob/main/cloud_converter/sand_filter_v2.py"""
+
+import os, sys
+from collections import defaultdict
+import re
+
+
+def main(datafile_in, datafile_out):
+
+    lines = []
+    param_line = []
+    param_current = []
+    parsing = False
+    param_dr = False
+    index_line = []
+    year_line = []
+    index_tag = []
+    year_tag = []
+
+    with open(datafile_in, 'r') as file_in:
+        for line in file_in:
+            line = line.strip('\t ').replace('\t', ' ')
+            line = re.sub(' +', ' ', line)
+            line = line.replace(' \n', '\n')
+
+            if line.startswith('set YEAR'):
+                start_year = line.split(':=')[1].split(' ')[1]
+                # print(start_year)
+
+            if line.startswith('param'):
+                parsing = True
+
+                if line.startswith('param REMinProductionTarget'):0
+                line = line.replace(':', ':=')
+
+                if line.startswith('param DiscountRate'):0
+                param_dr = True
+
+                if line.startswith('param ResultsPath'):
+                    continue
+
+
+            if parsing:
+                line_values = []
+                if line.startswith('param'):
+                    param_current = line.split(' ')[1]
+                    line_elements = list(line.split(' '))
+                    line_elements = [i.strip("\n:=") for i in line_elements]
+                    lines.append(line)
+
+                    if 'default' in line_elements:
+                        default_index = line_elements.index('default')  # Find position of 'default'
+                        default_value = line_elements[default_index+1]  # Extract default value
+                        # print(param_current, default_value)
+
+                elif line.startswith('[]'):
+                    index_line = line
+                    param_reset = True
+                    index_tag = True
+
+                elif line.startswith(str(start_year)):
+                    year_line = []
+                    year_line = line
+                    param_reset = True
+                    year_tag = True
+
+                else:
+                    line_values = list(set(line.rstrip('\n').split(' ')[1:-1]))
+
+                    if param_dr:
+                        if not line.startswith(';'):
+                            lines.append(line)
+                        param_dr = False
+
+                    if line_values != list(default_value):
+                        if line_values != []:
+                            param_line = line
+
+                    if len(param_line) > 0:
+                        if param_reset:
+                            if index_tag:
+                                if index_line != []:
+                                    lines.append(index_line)
+                            if year_tag:
+                                if year_line != []:
+                                    lines.append(year_line)
+                        param_reset = False
+                        if not param_reset:
+                            lines.append(param_line)
+                            param_line = []
+            if line.startswith(';'):
+                lines.append(line)
+                # print(param_current, index_tag, year_tag)
+                parsing = False
+                index_tag = False
+                year_tag = False
+            elif not parsing:
+                lines.append(line)
+
+    with open(datafile_out, 'w') as file_out:
+        file_out.writelines(lines)
+
+
+if __name__ == "__main__":
+
+    if "snakemake" in globals():
+        main(snakemake.input.txt, snakemake.output.txt)
+    else:
+        if len(sys.argv) != 3:
+            msg = "Usage: python {} <data_in> <data_out>"
+            print(msg.format(sys.argv[0]))
+            sys.exit(1)
+        else:
+            data_in=sys.argv[1]
+            data_out = sys.argv[2]
+            main(data_in, data_out)
+
