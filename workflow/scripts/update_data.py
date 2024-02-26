8ae727b9096a71be69aed9a193bb0bd6ab1d42d0
hannahluscombe <157520297+hannahluscombe@users.noreply.github.com>
2024-02-26 19:08:49 +0000

    Update Data

--- workflow/scripts/update_data.py
+++ workflow/scripts/update_data.py
@@ -5,16 +5,22 @@ import pandas as pd
 import sys
 from otoole import read, write
 
-def correct_data(data: Dict[str, pd.DataFrame]) -> pd.DataFrame:
+def correct_data(data: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
     """Corrects Discount Rate"""
-    # add logic here
-    pass
+    discount_rate = 0.05
+    df = data["DiscountRate"]
+    df["VALUE"] = discount_rate
+    data["DiscountRate"] = df
+    return data
 
 if __name__ == "__main__":
     if "snakemake" in globals():
-        data, defults = read() # add arguments to read function 
+        config = snakemake.inputs.config
+        data_in = snakemake.inputs.txt
+        data_out = snakemake.outputs.txt
+        data, defaults = read(config, 'datafile', data_in) 
         data = correct_data(data)
-        write() # add arguments to write function
+        write(config, 'datafile', data_out, data, defaults)
     else:
         if len(sys.argv) != 0: # replace 0 with the number of arguments you expect 
             msg = "Usage: python {}" # correct usage statement 
@@ -22,6 +28,6 @@ if __name__ == "__main__":
             sys.exit(1)
         else:
             # add parsing of arguments 
-            data, defults = read() # add arguments to read function 
+            data, defaults = read() # add arguments to read function 
             data = correct_data(data)
             write() # add arguments to write function
