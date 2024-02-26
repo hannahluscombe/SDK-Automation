436512051bdc9233a78a68d05897456a3370bb08
trevorb1 <trevor_barnes@sfu.ca>
2024-02-25 20:13:20 -0800

    refactor update data

--- workflow/scripts/update_data.py
+++ workflow/scripts/update_data.py
@@ -18,16 +18,16 @@ if __name__ == "__main__":
         config = snakemake.inputs.config
         data_in = snakemake.inputs.txt
         data_out = snakemake.outputs.txt
-        data, defaults = read(config, 'datafile', data_in) 
-        data = correct_data(data)
-        write(config, 'datafile', data_out, data, defaults)
     else:
-        if len(sys.argv) != 0: # replace 0 with the number of arguments you expect 
-            msg = "Usage: python {}" # correct usage statement 
+        if len(sys.argv) != 4: 
+            msg = "Usage: python {} <otoole_config.yaml> <in_datafile.txt> <out_datafile.txt>"
             print(msg.format(sys.argv[0]))
             sys.exit(1)
         else:
-            # add parsing of arguments 
-            data, defaults = read() # add arguments to read function 
-            data = correct_data(data)
-            write() # add arguments to write function
+            config = sys.argv[1]
+            data_in = sys.argv[2]
+            data_out = sys.argv[3]
+    
+    data, defaults = read(config, 'datafile', data_in) 
+    data = correct_data(data)
+    write(config, 'datafile', data_out, data, defaults)
